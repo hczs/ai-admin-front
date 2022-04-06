e4bc28bc2a483aed12bd8e62b654ff33e5d60d62
houcheng <houcheng@boco.com.cn>
2022-04-06 17:28:47 +0800

    注册消息提示

--- src/lang/zh.js
+++ src/lang/zh.js
@@ -126,7 +126,7 @@ export default {
     toRegister: '没有账号？点击注册',
     title: '用户注册',
     sendCodeError: '验证码发送失败',
-    registerError: '注册失败',
+    registerError: '注册失败，请检查验证码和邮箱是否正确！',
     registerSuccess: '注册成功',
     sendCodeSuccess: '验证码发送成功，请注意查收',
     mailRepeat: '此邮箱已注册过本系统'
